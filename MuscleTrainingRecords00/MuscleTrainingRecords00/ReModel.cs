b4001356b00fa935bb17fa58e4eadd45be77b23a
燎 石橋 <150461@jc-21.jp>
2018-02-05 18:58:21 +0900

    fix

--- MuscleTrainingRecords00/MuscleTrainingRecords00/ReModel.cs
+++ MuscleTrainingRecords00/MuscleTrainingRecords00/ReModel.cs
@@ -18,10 +18,10 @@ namespace MuscleTrainingRecords00
         
         public ReModel()
         {
-            if(RecordsModel.SelectRe().Equals(name))
+            /*if(RecordsModel.SelectRe().Equals(name))
             {
                 //RecordsModel.UpdateRe(date);
-            }
+            }*/
 
             if (RecordsModel.SelectRecords() != null)
             {
